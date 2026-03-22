0e7ac48208a1db526a554791176f5ae9cc4ad9f1
dat31122005 <8a1.nguyenminhdat31122005@gmail.com>
2026-03-22 23:18:35 +0700

    Fix checkpoint path and update print message
    Updated checkpoint path and modified print statement.

--- end-to-end/models/down_weight_model.py
+++ end-to-end/models/down_weight_model.py
@@ -2,7 +2,6 @@ import torch
 
 ckpt_path = r"D:\USTH\nlp\NLP_Emotion_Group_14\end-to-end\results\electra+bert\checkpoints\bert_stage2_emotion.pth" # <-- sửa path
 
-# Những key bạn muốn giữ
 KEEP_KEYS = {
     "epoch",
     "model_name",
@@ -15,20 +14,16 @@ KEEP_KEYS = {
     "threshold",
 }
 
-# Load
 ckpt = torch.load(ckpt_path, map_location="cpu")
 
-# Lọc key
 new_ckpt = {k: v for k, v in ckpt.items() if k in KEEP_KEYS}
 
-# (OPTION) giảm size: convert weight sang float16
 if "model_state" in new_ckpt:
     new_ckpt["model_state"] = {
         k: v.half() if torch.is_floating_point(v) else v
         for k, v in new_ckpt["model_state"].items()
     }
 
-# Save đè
 torch.save(new_ckpt, ckpt_path)
 
-print("✅ Done. Cleaned checkpoint saved.")
+print("Done. Cleaned checkpoint saved.")
